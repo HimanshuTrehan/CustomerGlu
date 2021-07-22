dec28d5658c11ef533998f13d5ac8cae5a73fa62
Himanshu <himanshutrehan2000@gmail.com>
2021-07-22 15:59:16 +0530

    Registration

--- Sources/CustomerGlu/CustomerGlu.swift
+++ Sources/CustomerGlu/CustomerGlu.swift
@@ -2,67 +2,59 @@ import Foundation
 import SwiftUI
 
 @available(iOS 13.0, *)
-public struct CustomerGlu {
+public class CustomerGlu {
     
     @available(iOS 13.0, *)
     public init(){
     }
     var text = "Hello World !"
+    var model = [RegistrationModel]()
+    var apidata = RegistrationModel()
+    var baseurl = "https://api.customerglu.com/user/v1/user/sdk?token=true"
     
-    public class CustomerGluRegistration:ObservableObject
-    {
-         var model = [RegistrationModel]()
-       @Published  var apidata=RegistrationModel()
-         var baseurl = "https://api.customerglu.com/user/v1/user/sdk?token=true"
-        
-        public init(){}
-        
-      public  func doRegister(body:Any,completion:@escaping (RegistrationModel)->Void) ->RegistrationModel
-        {
+    public func doRegister(body:Any,completion:@escaping (RegistrationModel)->Void) ->RegistrationModel
+      {
 
-            let jsonData = try! JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+          let jsonData = try! JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
 
-             let myurl = URL(string: baseurl)
-            var request = URLRequest(url: myurl!)
-            request.httpMethod="POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+           let myurl = URL(string: baseurl)
+          var request = URLRequest(url: myurl!)
+          request.httpMethod="POST"
+          request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
-            request.httpBody = jsonData
+          request.httpBody = jsonData
 
-            URLSession.shared.dataTask(with: request) { data, response, error in
-                
-                if error == nil && data != nil
-                {
-                    do {
-                        let dictonary = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
-                        as?[String:Any]
-                        print(dictonary as Any)
-                        do{
-                            let mydata = try JSONDecoder().decode(RegistrationModel.self, from: data!)
-                            print("data")
-                            DispatchQueue.main.async{
-                                self.apidata = mydata
-                                completion(self.apidata)
-                            }
-                            
-                        }
-                        catch
-                        {
-                            print("json parsing error:\(error)")
-                        }
+          URLSession.shared.dataTask(with: request) { data, response, error in
+              
+              if error == nil && data != nil
+              {
+                  do {
+                      let dictonary = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                      as?[String:Any]
+                      print(dictonary as Any)
+                      do{
+                          let mydata = try JSONDecoder().decode(RegistrationModel.self, from: data!)
+                          print("data")
+                          DispatchQueue.main.async{
+                            self.apidata = mydata
+                              completion(self.apidata)
+                          }
+                          
+                      }
+                      catch
+                      {
+                          print("json parsing error:\(error)")
+                      }
 
-                    } catch  {
-                        print("error: \(error)")
-                    }
-                }
-             
-                
-            }.resume()
-            
-            return self.apidata
-        }
-      
+                  } catch  {
+                      print("error: \(error)")
+                  }
+              }
+           
+              
+          }.resume()
+          
+          return self.apidata
+      }
     
-    
-}
 }
