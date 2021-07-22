6b97bc257f035a6d807340f47ff4837bcd2f0d19
Himanshu <himanshutrehan2000@gmail.com>
2021-07-22 13:38:46 +0530

    f

--- Tests/CustomerGluTests/CustomerGluTests.swift
+++ Tests/CustomerGluTests/CustomerGluTests.swift
@@ -2,6 +2,7 @@
     @testable import CustomerGlu
 
     final class CustomerGluTests: XCTestCase {
+        @available(iOS 13.0, *)
         func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
