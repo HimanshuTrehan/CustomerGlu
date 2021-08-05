9a12e173358c9424f5a12100165ef8167a7d931a
Himanshu <himanshutrehan2000@gmail.com>
2021-08-05 13:38:59 +0530

    stack

--- Sources/CustomerGlu/View/LoadAllCampaigns.swift
+++ Sources/CustomerGlu/View/LoadAllCampaigns.swift
@@ -52,37 +52,41 @@ public struct LoadAllCampaigns: View {
                   {
                       element in
                       
-                      if element.banner != nil
-                      {
-                      if element.banner?.imageUrl == nil && element.banner?.title == nil
-                              {
-                          BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: "",url:element.url )
-                               }
-                          
-                         else if element.banner?.imageUrl == nil{
-                          BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: (element.banner?.title)!,url: element.url)
-                          }
-                          
-                          else if element.banner?.title == nil
-                          {
-                              BannerCell(image_url: (element.banner?.imageUrl!)!, title:"",url: element.url)
-                          }
-                          else{
-                              BannerCell(image_url: (element.banner?.imageUrl!)!, title:(element.banner?.title!)!,url: element.url)
-                              
-                          }
+                ZStack(alignment:.leading)
+                {
+                    if element.banner != nil
+                    {
+                    if element.banner?.imageUrl == nil && element.banner?.title == nil
+                            {
+                        BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: "",url:element.url )
+                             }
+                        
+                       else if element.banner?.imageUrl == nil{
+                        BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: (element.banner?.title)!,url: element.url)
+                        }
+                        
+                        else if element.banner?.title == nil
+                        {
+                            BannerCell(image_url: (element.banner?.imageUrl!)!, title:"",url: element.url)
+                        }
+                        else{
+                            BannerCell(image_url: (element.banner?.imageUrl!)!, title:(element.banner?.title!)!,url: element.url)
+                            
+                        }
 
-                      }
-                      else
-                      {
-              BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: "",url: element.url)
-                          
-                    
-                       
-                      }
+                    }
+                    else
+                    {
+            BannerCell(image_url: "https://images.unsplash.com/photo-1614680376739-414d95ff43df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWVzJTIwYmFubmVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60", title: "",url: element.url)
+                        
+                  
+                     
+                    }
+                }
             
 
             }.background(Color.white)
+            
             Spacer()
 
             
