2d1a1a8449a8ae523bf0b0be25f17247d4b3da6d
Himanshu <himanshutrehan2000@gmail.com>
2021-08-16 15:11:46 +0530

    public

--- Sources/CustomerGlu/CustomerGlu.swift
+++ Sources/CustomerGlu/CustomerGlu.swift
@@ -101,7 +101,7 @@ public class CustomerGlu:ObservableObject {
         }.resume()
         
     }
-    func openWallet() {
+  public func openWallet() {
 
         let swiftUIView = OpenWallet(cus_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJnbHUiLCJnbHVJZCI6IjIzZmRkOWU0LWRjYWMtNDlkYS1hMWI5LThmYWRjOGU2YWZkNSIsImNsaWVudCI6Ijg0YWNmMmFjLWIyZTAtNDkyNy04NjUzLWNiYTJiODM4MTZjMiIsImRldmljZUlkIjoiZGV2aWNlYiIsImRldmljZVR5cGUiOiJhbmRyb2lkIiwiaWF0IjoxNjI4MDY0NzYxLCJleHAiOjE2NTk2MDA3NjF9.x86E0pKN2n_nc_If6nqL3CsxGql78q4ehtgwbUdaAwg",fromKit: true)
     //UIHostingController
