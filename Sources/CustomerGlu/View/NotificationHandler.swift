109186f3f24a02c2fc1133e802295b2337cdec96
Himanshu <himanshutrehan2000@gmail.com>
2021-08-29 13:47:44 +0530

    ew

--- Sources/CustomerGlu/View/NotificationHandler.swift
+++ Sources/CustomerGlu/View/NotificationHandler.swift
@@ -1,6 +1,16 @@
 //
-//  SwiftUIView.swift
+//  File.swift
 //  
+//
+//  Created by Himanshu Trehan on 29/08/21.
+//
+
+import Foundation
+
+
+//
+//  SwiftUIView.swift
+//
 //
 //  Created by Himanshu Trehan on 24/07/21.
 //
@@ -10,21 +20,19 @@ import SwiftUI
 import UIKit
 @available(iOS 13.0, *)
 
-public struct OpenWallet: View {
+public struct NotificationHandler: View {
     
-    @State var token:String
     @State var fromUikit = false
-    @State var my_url=""
+    @State var my_url:String
 
-    public init(cus_token:String,fromKit:Bool = false){
-        token = cus_token
+    public init(fromKit:Bool = false){
         fromUikit = fromKit
     }
   
    public var body: some View {
         VStack
         {
-            if my_url=="" 
+            if my_url==""
             {
                 EmptyView()
             }
@@ -33,25 +41,12 @@ public struct OpenWallet: View {
                 CustomerWebView(my_url: my_url,fromWallet: true,fromUikit: false)
             }
         }
-        .onAppear(perform: getCampaigns)
         .navigationViewStyle(StackNavigationViewStyle())
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
    }
-//    public func closeView()
-//    {
-//        self.presentationMode.wrappedValue.dismiss()
-//    }
+
     
-    
-   
-    public func getCampaigns()
-     {
-        
-        CustomerGlu().retrieveData(customer_token: token) { CampaignsModel in
-             my_url = CampaignsModel.defaultUrl
-            print(my_url)
-        }
-     }
+
 }
 #endif
