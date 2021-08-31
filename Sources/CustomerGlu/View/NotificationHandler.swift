ed32e40521472c1edea1718baa6988ab64533cb1
Himanshu <himanshutrehan2000@gmail.com>
2021-08-31 13:12:04 +0530

    middle

--- Sources/CustomerGlu/View/NotificationHandler.swift
+++ Sources/CustomerGlu/View/NotificationHandler.swift
@@ -24,14 +24,41 @@ import UIKit
     
     @State var fromUikit = false
     @State var my_url:String
-
+    @State var ismiddle = false
   
     var body: some View {
         VStack
         {
-        
+            if my_url==""
+            {
+                EmptyView()
+                
+            }
+            else
+            {
+                if ismiddle
+                {
+                    ZStack
+                    {
+                        // PopUp background color
+                                       Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+
+                                       // PopUp Window
+                                       VStack(alignment: .center, spacing: 0) {
+                                        CustomerWebView(my_url: my_url,fromWallet: true,fromUikit: true)
+
+                                       }
+                                       .frame(maxWidth: 300)
+                                       .border(Color.white, width: 2)
+                                   //    .background(Color(#colorLiteral(red: 0.737254902, green: 0.1294117647, blue: 0.2941176471, alpha: 1)))
+                                   }
+                    }
+                
+                else
+                {
                 CustomerWebView(my_url: my_url,fromWallet: true,fromUikit: true)
-            
+                }
+            }
         }
         .navigationViewStyle(StackNavigationViewStyle())
         .navigationBarBackButtonHidden(true)
