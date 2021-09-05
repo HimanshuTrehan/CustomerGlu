71b06ae9d42563ee147075cb1be916e516ace063
Himanshu <himanshutrehan2000@gmail.com>
2021-09-05 15:38:39 +0530

    nb

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "CustomerGlu",
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -25,4 +28,5 @@ let package = Package(
             name: "CustomerGluTests",
             dependencies: ["CustomerGlu"]),
     ]
+    
 )
